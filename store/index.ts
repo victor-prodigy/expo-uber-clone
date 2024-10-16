e2472ce99c5165a6f2dfac4e1f69f9f763eb2619
note <victor.numair@pucpr.edu.br>
2024-10-15 23:58:39 -0300

    first commit

---
+++ store/index.ts
@@ -0,0 +1,62 @@
+// NOTE: store ou hooks
+import { create } from "zustand";
+
+import { DriverStore, LocationStore, MarkerData } from "@/types/type";
+
+export const useLocationStore = create<LocationStore>((set) => ({
+  userLatitude: null,
+  userLongitude: null,
+  userAddress: null,
+  destinationLatitude: null,
+  destinationLongitude: null,
+  destinationAddress: null,
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      userLatitude: latitude,
+      userLongitude: longitude,
+      userAddress: address,
+    }));
+
+    // if driver is selected and now new location is set, clear the selected driver
+    const { selectedDriver, clearSelectedDriver } = useDriverStore.getState();
+    if (selectedDriver) clearSelectedDriver();
+  },
+
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+    }));
+
+    // if driver is selected and now new location is set, clear the selected driver
+    const { selectedDriver, clearSelectedDriver } = useDriverStore.getState();
+    if (selectedDriver) clearSelectedDriver();
+  },
+}));
+
+// https://youtu.be/kmy_YNhl0mw?t=11942
+export const useDriverStore = create<DriverStore>((set) => ({
+  drivers: [] as MarkerData[],
+  selectedDriver: null,
+  setSelectedDriver: (driverId: number) =>
+    set(() => ({ selectedDriver: driverId })),
+  setDrivers: (drivers: MarkerData[]) => set(() => ({ drivers })),
+  clearSelectedDriver: () => set(() => ({ selectedDriver: null })),
+}));
